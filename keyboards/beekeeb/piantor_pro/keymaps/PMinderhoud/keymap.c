246523313da5dc02da3fe6c9697bb9897ecbce1d
Pascal Minderhoud <github@goduckygo.33mail.com>
2024-01-01 15:48:05 +0100

    Added caps word

--- keyboards/beekeeb/piantor_pro/keymaps/PMinderhoud/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/PMinderhoud/keymap.c
@@ -4,102 +4,11 @@
 #include "keycodes.h"
 #include "keymap_us.h"
 #include "quantum_keycodes.h"
+#include "keymap.h"
 #include QMK_KEYBOARD_H
 #include "features/select_word.h"
 #include "features/achordion.h"
-
-// Enums
-enum layers {
-    BASE,
-    BASE_NOMOD,
-    QWERTY,
-    DIACRIT,
-    FUNC,
-    SYM,
-    NAV,
-    ADJUST,
-};
-
-enum custom_keycodes {
-    PK_ISNOT = SAFE_RANGE,
-    SELWORD,
-};
-
-enum unicode_names {
-  U_SS_LOWER,
-  U_SS_UPPER,
-  U_NTIL_LOWER,
-  U_NTIL_UPPER,
-  U_CCED_LOWER,
-  U_CCED_UPPER,
-  U_IQUE_SYM,
-  U_A_TREMA_L,
-  U_A_TREMA_U,
-  U_A_AIGU_L,
-  U_A_AIGU_U,
-  U_A_GRAVE_L,
-  U_A_GRAVE_U,
-  U_E_TREMA_L,
-  U_E_TREMA_U,
-  U_E_AIGU_L,
-  U_E_AIGU_U,
-  U_E_GRAVE_L,
-  U_E_GRAVE_U,
-  U_E_CIRC_L,
-  U_E_CIRC_U,
-  U_I_TREMA_L,
-  U_I_TREMA_U,
-  U_I_AIGU_L,
-  U_I_AIGU_U,
-  U_I_CIRC_L,
-  U_I_CIRC_U,
-  U_O_TREMA_L,
-  U_O_TREMA_U,
-  U_O_AIGU_L,
-  U_O_AIGU_U,
-  U_O_CIRC_L,
-  U_O_CIRC_U,
-  U_U_TREMA_L,
-  U_U_TREMA_U,
-  U_U_AIGU_L,
-  U_U_AIGU_U,
-  U_U_CIRC_L,
-  U_U_CIRC_U,
-  U_EURO,
-};
-
-// Home row mods for Base (Recurva) layer.
-#define HOME_S LGUI_T(KC_S)
-#define HOME_N LALT_T(KC_N)
-#define HOME_T LCTL_T(KC_T)
-#define HOME_C LSFT_T(KC_C)
-#define HOME_H RSFT_T(KC_H)
-#define HOME_E RCTL_T(KC_E)
-#define HOME_A LALT_T(KC_A)
-#define HOME_I RGUI_T(KC_I)
-
-// Home row mods for QWERTY layer.
-#define QHOME_A LGUI_T(KC_A)
-#define QHOME_S LALT_T(KC_S)
-#define QHOME_D LSFT_T(KC_D)
-#define QHOME_F LCTL_T(KC_F)
-#define QHOME_J RCTL_T(KC_J)
-#define QHOME_K RSFT_T(KC_K)
-#define QHOME_L LALT_T(KC_L)
-#define QHOME_SC RGUI_T(KC_SCLN)
-
-#define DHOME_C LSFT_T(U_CCED)
-#define DHOME_O RSFT_T(U_OTREM)
-
-#define SHOME_EX LGUI_T(KC_EXLM)
-#define SHOME_LT LALT_T(KC_LABK)
-#define SHOME_GT LCTL_T(KC_RABK)
-#define SHOME_EQ LSFT_T(KC_EQL)
-
-#define NHOME_CZ LGUI_T(C(KC_Z))
-#define NHOME_CX LALT_T(C(KC_X))
-#define NHOME_CC LCTL_T(C(KC_C))
-#define NHOME_CV LSFT_T(C(KC_V))
+#include "features/caps_word.h"
 
 // Mappings
 const uint32_t unicode_map[] PROGMEM = {
@@ -145,39 +54,6 @@ const uint32_t unicode_map[] PROGMEM = {
   [U_EURO]       = 0x20ac,  // €
 };
 
-#define U_SS UP(U_SS_LOWER, U_SS_UPPER)
-// ñ and Ñ keycode.
-#define U_NTIL UP(U_NTIL_LOWER, U_NTIL_UPPER)
-// ç and Ç keycode.
-#define U_CCED UP(U_CCED_LOWER, U_CCED_UPPER)
-#define U_ATREM UP(U_A_TREMA_L, U_A_TREMA_U)
-#define U_AAIGU UP(U_A_AIGU_L, U_A_AIGU_U)
-#define U_AGRAV UP(U_A_GRAVE_L, U_A_GRAVE_L)
-#define U_ETREM UP(U_E_TREMA_L, U_E_TREMA_U)
-#define U_EAIGU UP(U_E_AIGU_L, U_E_AIGU_U)
-#define U_EGRAV UP(U_E_GRAVE_L, U_E_GRAVE_U)
-#define U_ECIRC UP(U_E_CIRC_L, U_E_CIRC_U)
-#define U_ITREM UP(U_I_TREMA_L, U_I_TREMA_U)
-#define U_IAIGU UP(U_I_AIGU_L, U_I_AIGU_U)
-#define U_ICIRC UP(U_I_CIRC_L, U_I_CIRC_U)
-#define U_OTREM UP(U_O_TREMA_L, U_O_TREMA_U)
-#define U_OAIGU UP(U_O_AIGU_L, U_O_AIGU_U)
-#define U_OCIRC UP(U_O_CIRC_L, U_O_CIRC_U)
-#define U_UTREM UP(U_U_TREMA_L, U_U_TREMA_U)
-#define U_UAIGU UP(U_U_AIGU_L, U_U_AIGU_U)
-#define U_UCIRC UP(U_U_CIRC_L, U_U_CIRC_U)
-
-#define L_SP_NAV LT(NAV,KC_SPC)
-#define L_BS_SYM LT(SYM,KC_BACKSPACE)
-#define L_RA_DIA LT(DIACRIT,KC_RALT)
-#define L_TB_FNC LT(FUNC,KC_TAB)
-
-#define SK_NWDSK G(C(KC_D))     // New desktop
-#define SK_CLDSK G(C(KC_F4))     // Close desktop
-#define SK_PRDSK G(C(KC_LEFT))     // Previous desktop
-#define SK_NXDSK G(C(KC_RIGHT))     // Next desktop
-
-#define RC(kc) RCTL(kc)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -257,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [FUNC] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      DM_REC1, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DM_PLY1,                      XXXXXXX,   KC_F9,  KC_F10,  KC_F11,  KC_F12, MO(ADJUST),
+      DM_REC1,CONSTCSE,CAMELCSE,PASCLCSE,CAPSWORD, DM_PLY1,                      XXXXXXX,   KC_F9,  KC_F10,  KC_F11,  KC_F12, MO(ADJUST),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       DM_REC2, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, DM_PLY2,                      XXXXXXX,   KC_F5,   KC_F6,   KC_F7,   KC_F8, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -325,6 +201,31 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
         if (record->event.pressed) {
             SEND_STRING(" is not ");
         }
+        break;
+    case CAPSWORD:
+        if (record->event.pressed) {
+            toggle_caps_word_mode(CAPS_WORD_MODE_DEFAULT);
+            return false;
+        }
+        break;
+    case CAMELCSE:
+        if (record->event.pressed) {
+            toggle_caps_word_mode(CWMODE_CAMEL_CASE);
+            return false;
+        }
+        break;
+    case PASCLCSE:
+        if (record->event.pressed) {
+            toggle_caps_word_mode(CWMODE_PASCAL_CASE);
+            return false;
+        }
+        break;
+    case CONSTCSE:
+        if (record->event.pressed) {
+            toggle_caps_word_mode(CWMODE_CONSTANT_CASE);
+            return false;
+        }
+        break;
   }
 
   return true;
